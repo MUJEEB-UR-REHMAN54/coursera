6d71c6de8682f443b88d5ddbbd5dea102d25358f
MUJEEB-UR-REHMAN54 <mujeebrehmann603@gmail.com>
2021-08-25 09:52:22 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ function sendMessage() {
     var _Email = document.getElementById("Email").value;
     var _Message_Body = document.getElementById("msg").value;
     var _Full_Name = _First_Name + " " + _Last_Name;
-    var _Message = "<div>" + _Full_Name +
-    ":</div><div>" + _Message_Body +
-    "</div><div>Email: " + _Email + 
-    "</div><div>Contact Number: " + _Phone_Number + "</div>";
+    var _Message = "<div><b>" + _Full_Name +
+    ":</b></div><div>" + _Message_Body +
+    "</div><div><b>Email:</b> " + _Email + 
+    "</div><div><b>Contact Number</b>: " + _Phone_Number + "</div>";
     var Message_Html = document.getElementById("messages");
     Message_Html.innerHTML = Message_Html.innerHTML + _Message;
 }
